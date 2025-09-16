cf16ec5412fe918a06f91b3f692e861da4f276b6
METADEV <80870883+metadev999@users.noreply.github.com>
2025-09-16 21:00:58 +0800

    qweqwe

--- _next/static/chunks/app/page-25616cffa592c35f.js
+++ _next/static/chunks/app/page-25616cffa592c35f.js
@@ -131,7 +131,7 @@
                                                     children: "Total Supply"
                                                 }), (0, n.jsx)("span", {
                                                     className: "sm:text-base text-sm font-semibold text-white px-5",
-                                                      children: (0, p.GM)(String((0, p.h_)(Number(t), 6))) || (0, n.jsx)("span", {
+                                                      children: (0, p.GM)(String(Number("1000000000"))) || (0, n.jsx)("span", {
                                                         className: "inline-block h-4 w-40 animate-pulse rounded bg-gray-700"
                                                     })
                                                 })]
